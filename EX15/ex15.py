fd53934ec99ca23c73875cf2f0d0f48231d48382
sonjagrusche <sonja.grusche@stud.leuphana.de>
2016-10-25 17:25:28 +0200

    EX15 not done yet

---
+++ EX15/ex15.py
@@ -0,0 +1,29 @@
+# importing the module argv from sys (package)
+from sys import argv
+
+# multiple assignment of arguments
+script, filename = argv
+
+# setting a command to open variable files
+txt = open(filename)
+
+# this line uses a format character to fill in the filename we gave as an argument
+print "Here's your file %r:" % filename
+# this line uses the variable we set in line 8 to open it and print it (reading it out)
+print txt.read()
+
+print "Type the file name again:"
+# using the raw_input to get input by the user
+file_again = raw_input("> ")
+
+# what the user types in in line 17 will be opened at this point (if it exists)
+txt_again = open(file_again)
+# same function as line 13
+print txt_again.read()
+
+
+# commandsare also called "functions" or "methods"
+
+# Study Drill
+# raw_input will use any input you give,
+# whereas argv needs rather specific arguments (?)
