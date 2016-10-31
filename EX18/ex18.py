231064eadb38dfcd405e928989ae3b89d25988e1
sonjagrusche <sonja.grusche@stud.leuphana.de>
2016-10-31 14:32:32 +0100

    EX20is still in progress

---
+++ EX18/ex18.py
@@ -0,0 +1,26 @@
+# this one is like your scripts with argv
+# telling Python to make a function using def (define)
+# "print_two" is just a name foor the function, could be any other name
+# *args is pretty much the same as argv for functions
+def print_two(*args):
+    # everything indented after the colon will be attached to"print_two"
+    # first ind line unpacksthe arguments (just like for scripts)
+    arg1, arg2 = args
+    print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+# ok, that *args is actually pointless, we can just do this
+def print_two_again(arg1, arg2):
+    print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+# this just takes one argument
+def print_one(arg1):
+    print "arg1: %r" % arg1
+
+# this one  takesno arguments
+def print_none():
+    print "I got nothin'."
+
+print_two("Zed","Shaw")
+print_two_again("Zed","Shaw")
+print_one("First!")
+print_none()
---
+++ EX19/ex19.py
@@ -0,0 +1,59 @@
+#def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    #print "You have %d cheeses!" % cheese_count
+    #print "You have %d boxes of crackers!" % boxes_of_crackers
+    #print "Man that's enough for a party!"
+    #print "Get a blanket.\n"
+
+
+#print "We can just give the function number directly:"
+#cheese_and_crackers(20, 30)
+
+#print "OR, we can use variables from our script:"
+#amount_of_cheese = 10
+#amount_of_crackers = 50
+
+#cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+
+#print "We can even do math inside too:"
+#cheese_and_crackers(10 + 20, 5 + 6)
+
+
+#print "And we can combine the two, variables and math:"
+#cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
+
+# Study Drill
+def popcorn_and_icecream(popcorn_buckets, icecream_sandwiches):
+    print "I have %r buckets of popcorn!" % popcorn_buckets
+    print "And look, in the freezer are %r icecream sandwiches!" % icecream_sandwiches
+    print "Let's Netflix 'n' chill. \n"
+
+
+print "Version 1: giving numbers directly:"
+popcorn_and_icecream(3, 6)
+
+print "Version 2: using variables:"
+amount_of_buckets = 4
+amount_of_sandwiches = 5
+
+popcorn_and_icecream(amount_of_buckets, amount_of_sandwiches)
+
+print "Version 3: math:"
+popcorn_and_icecream(20 - 15, 2 * 3)
+
+print "Version 4: math and variables:"
+popcorn_and_icecream(amount_of_buckets + 4, amount_of_sandwiches - 2)
+
+print "Versoin 5: user:"
+print "What do you think, how many buckets of popcorn are there?"
+buckets = raw_input()
+print "And how many icecream sandwiches are left?"
+sandwiches = raw_input()
+popcorn_and_icecream(buckets, sandwiches)
+
+print "Version 6: user guided by prompt:"
+popcornbuckets = raw_input("How many popcorn buckets do you have?")
+icecreamsandwiches = raw_input("And how many sandwiches?")
+popcorn_and_icecream(popcornbuckets, icecreamsandwiches)
+
+print "Ok, I can't think of more versions right now."
